148d30dbee218695f9e24a7c1de080a34f8ed9b9
Lorena Sambad Lema <lsambadlema@gmail.com>
2024-10-31 15:55:57 +0100

    Terminado Exercise27

--- src/main/java/com/campusdual/classroom/Exercise27.java
+++ src/main/java/com/campusdual/classroom/Exercise27.java
@@ -1,8 +1,18 @@
 package com.campusdual.classroom;
 
+import com.campusdual.classroom.ShoppingListJSONCreator;
+import com.campusdual.xml.ShoppingListXMLCreator;
 
 public class Exercise27 {
     public static void main(String[] args) {
+        try {
+            ShoppingListXMLCreator.createDocument();
+            System.out.println("Archivo XML creado exitosamente.");
 
+            ShoppingListJSONCreator.createDocument();
+            System.out.println("Archivo JSON creado exitosamente.");
+        } catch (Exception e) {
+            System.err.println("Error al crear los archivos: " + e.getMessage());
+        }
     }
 }
